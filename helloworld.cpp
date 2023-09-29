02b9d003203a47d34815d67825fa01a5eff3c797
Amit <akkmit9@gmail.com>
2023-09-29 21:05:36 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+    int main(){
+        cout<<"Hello World!!";
+        return 0;
+    }
+    //write a programm to create object and classes in c++ 
+    
